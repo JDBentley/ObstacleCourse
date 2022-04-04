764ee1683870bfb8b7314932391aecb4cb62b0d1
Jesse Bentley <75673224+JDBentley@users.noreply.github.com>
2022-04-03 22:45:53 -0400

    Movement Speed Update
    Updated movement speed to include Delta Time as to avoid FPS limits.

--- ObstacleCourseSource/Assets/Scripts/Mover.cs
+++ ObstacleCourseSource/Assets/Scripts/Mover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +14,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.Translate(xValue, 0f, zValue);
     }
 }
